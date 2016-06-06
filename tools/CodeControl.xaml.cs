04c21b17fa853ebf1cd8b192ca9311370578ad20
aoi-umi <1641622365@qq.com>
2016-06-06 15:37:45 +0800

    增加base64加密解密

--- tools/CodeControl.xaml.cs
+++ tools/CodeControl.xaml.cs
@@ -1,21 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ICSharpCode.AvalonEdit.Folding;
-using ICSharpCode.AvalonEdit;
-using ICSharpCode.AvalonEdit.Highlighting;
-using ICSharpCode.AvalonEdit.Document;
 using System.Windows.Threading;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Folding;
+using ICSharpCode.AvalonEdit.Highlighting;
 
 namespace tools
 {
---
+++ tools/EncodeDecodeControl.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tools
+{
+    /// <summary>
+    /// EncodeDecodeControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class EncodeDecodeControl : UserControl
+    {
+        public EncodeDecodeControl()
+        {
+            InitializeComponent();
+        }
+
+        private string DecodeString
+        {
+            get { return DecodeBox.Text; }
+            set { DecodeBox.Text = value; }
+        }
+
+        private string EncodeString
+        {
+            get { return EncodeBox.Text; }
+            set { EncodeBox.Text = value; }
+        }
+
+        private void DecodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem ti = Tab.SelectedItem as TabItem;
+            if (ti != null)
+            {
+                string output = string.Empty;
+                switch (ti.Name)
+                {
+                    case "base64":
+                        output = Base64(DecodeString, true);
+                        break;
+                    
+                }
+                EncodeString = output;
+            }
+        }
+
+        private void EncodeButton_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem ti = Tab.SelectedItem as TabItem;
+            if (ti != null)
+            {
+                string output = string.Empty;
+                switch (ti.Name)
+                {
+                    case "base64":
+                        output = Base64(EncodeString, false);
+                        break;
+
+                }
+                DecodeString = output;
+            }
+        }
+
+        private string Base64(string input, bool decode)
+        {
+            try
+            {
+                if (decode)
+                {
+                    return Encoding.Default.GetString(Convert.FromBase64String(input));
+                }
+                else
+                {
+                    return Convert.ToBase64String(Encoding.Default.GetBytes(input));
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
