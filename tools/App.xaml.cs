d265f6c7ee6030e5921cddf64d21a57309cf61e8
aoi-umi <1641622365@qq.com>
2016-09-06 18:38:01 +0800

    +全局处理未捕获的异常

--- tools/App.xaml.cs
+++ tools/App.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace tools
 {
@@ -12,5 +9,22 @@ namespace tools
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            var result = MessageBox.Show(
+                string.Format("{0}{1}{1}{2}", e.Exception.ToString(), Environment.NewLine, "是否退出?"),
+                "未处理错误",
+                MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                Shutdown();
+            }
+            e.Handled = true;
+        }
     }
 }
