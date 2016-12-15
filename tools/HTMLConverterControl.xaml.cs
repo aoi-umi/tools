915e8c043d326792a6d3e23d765651192e5a7d55
aoi-umi <1641622365@qq.com>
2016-12-15 18:43:08 +0800

    正则,高亮偏移处理。。。

--- tools/HTMLConverterControl.xaml.cs
+++ tools/HTMLConverterControl.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Markup;
 using HTMLConverter;
+using System.Collections.Generic;
 
 namespace tools
 {
@@ -17,13 +18,9 @@ namespace tools
             InitializeComponent();
         }
 
-        private void HtmlBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            
-        }
-
         private void HtmlToXaml_Click(object sender, RoutedEventArgs e)
         {
+            OutputBox.Text = string.Empty;
             string output = string.Empty;
             try
             {
@@ -40,14 +37,52 @@ namespace tools
 
         private void PreviewOnRichTextBox_Click(object sender, RoutedEventArgs e)
         {
+            OutputBox.Text = string.Empty;
             try
             {
                 PreviewRichTextBox.Document = XamlReader.Parse(XamlBox.Text) as FlowDocument;
+                var start = PreviewRichTextBox.Document.ContentStart;
+                PreviewTreeView.ItemsSource = GetTreeItem(PreviewRichTextBox.Document, ref start);
+
             }
             catch (Exception ex)
             {
                 OutputBox.Text = ex.ToString();
             }
         }
+
+        private List<TreeViewItemModel> GetTreeItem(FlowDocument document,ref TextPointer tp)
+        {
+            var list = new List<TreeViewItemModel>();
+            var start = tp;
+            while (start != null && document.ContentEnd.CompareTo(start) > -1)
+            {
+                var model = new TreeViewItemModel();
+                var context = start.GetPointerContext(LogicalDirection.Forward);
+                //model.Header = context.ToString() + ":";
+                switch (context)
+                {
+                    case TextPointerContext.ElementStart:
+                        model.Header = context.ToString() + ":";
+                    //    start = start.GetNextContextPosition(LogicalDirection.Forward);
+                    //    model.Items = GetTreeItem(document ,ref start);
+                        list.Add(model);
+                        break;
+                    //case TextPointerContext.None:
+                    //case TextPointerContext.ElementEnd:
+                    //    return list;
+                    case TextPointerContext.EmbeddedElement:
+                    case TextPointerContext.Text:
+                    //default:
+                        model.Header = context.ToString() + ":";
+                        model.Header += start.GetTextInRun(LogicalDirection.Forward);
+                        list.Add(model);
+                        break;
+                }
+                start = start.GetNextContextPosition(LogicalDirection.Forward);
+            }
+            return list;
+        }
+
     }
 }
--- tools/RegexControl.xaml.cs
+++ tools/RegexControl.xaml.cs
@@ -26,7 +26,7 @@ namespace tools
             {
                 if (InputBox.Document == null) return string.Empty;
                 TextRange documentRange = new TextRange(InputBox.Document.ContentStart, InputBox.Document.ContentEnd);
-                return documentRange.Text.Trim();
+                return documentRange.Text;
             }
             set
             {
@@ -63,15 +63,17 @@ namespace tools
         private void Input_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (string.IsNullOrEmpty(RegexString) || string.IsNullOrEmpty(InputString)) return;
+            InputBox.TextChanged -= Input_TextChanged;
             try
             {
-                InputBox.TextChanged -= Input_TextChanged;
                 Match m = Regex.Match(InputString, RegexString);
                 string output = string.Empty;
                 dataList.Clear();
                 m_tags.Clear();
                 TextRange documentRange = new TextRange(InputBox.Document.ContentStart, InputBox.Document.ContentEnd);
                 documentRange.ClearAllProperties();
+
+                InputTreeView.ItemsSource = GetTreeItem(InputBox.Document);
                 CheckWordsInFlowDocument(InputBox.Document);
 
                 for (int i = 0; i < m_tags.Count; i++)
@@ -79,7 +81,6 @@ namespace tools
                     TextRange range = new TextRange(m_tags[i].StartPosition, m_tags[i].EndPosition);
                     range.ApplyPropertyValue(TextElement.BackgroundProperty, i % 2 == 0 ? highlightBackground1 : highlightBackground2);
                 }
-
                 OutputString = DateTime.Now.ToString("HH:mm:ss") + "\r\n共" + dataList.Count + "个匹配项";
             }
             catch (Exception ex)
@@ -98,18 +99,21 @@ namespace tools
         }
 
         List<Tag> m_tags = new List<Tag>();
-        internal void CheckWordsInFlowDocument(FlowDocument flowDocument) //do not hightlight keywords in this method
+        void CheckWordsInFlowDocument(FlowDocument flowDocument) //do not hightlight keywords in this method
         {
             TextPointer navigator = flowDocument.ContentStart;
-            
+
             var text = InputString;
             if (string.IsNullOrEmpty(text)) return;
             Match match = Regex.Match(text, RegexString);
             int matchNum = 1;
+            int hadReadLength = 0;
             while (match.Success)
             {
                 TextPointer start = InputBox.Document.ContentStart;
-                var startPos = match.Index;
+                string word = match.Groups[0].ToString();
+                int startPos = match.Index, endPos = startPos + word.Length;
+
                 var model = new TreeViewItemModel() { Index = startPos, Header = "match" + matchNum + ",Index(" + startPos.ToString() + ")" };
                 int groupNum = 0;
                 foreach (var g in match.Groups)
@@ -120,56 +124,137 @@ namespace tools
                 ++matchNum;
                 dataList.Add(model);
 
-                string trueSearchstring = match.Groups[0].ToString();
-                int position = text.IndexOf(trueSearchstring);
-                string word = text.Substring(position, trueSearchstring.Length);
                 var textLength = 0;
-                var startOffset = 0;
-                
+                var startOffset = startPos;
+                var endOffset = endPos;
+                Tag t = new Tag();
+                t.Word = word;
+                while (start != null && flowDocument.ContentEnd.CompareTo(start) > -1 && textLength < startPos)
+                {
+                    var context = start.GetPointerContext(LogicalDirection.Forward);
+                    if (context != TextPointerContext.Text)
+                    {
+                        start = start.GetNextContextPosition(LogicalDirection.Forward);
+                        continue;
+                    }
+                    int len = start.GetTextRunLength(LogicalDirection.Forward);
+                    textLength += len;
+                    if(textLength < startPos) startOffset -= len;
+                }
+                t.StartPosition = start.GetPositionAtOffset(startOffset, LogicalDirection.Forward);
+
+                while (start != null && flowDocument.ContentEnd.CompareTo(start) > -1 && textLength < startOffset + word.Length)
+                {
+                    var context = start.GetPointerContext(LogicalDirection.Forward);
+                    if (context != TextPointerContext.Text)
+                    {
+                        start = start.GetNextContextPosition(LogicalDirection.Forward);
+                        continue;
+                    }
+                    int len = start.GetTextRunLength(LogicalDirection.Forward);
+                    textLength += len;
+                    if(textLength < startPos + word.Length) endOffset -= len;
+                }
+                t.EndPosition = start.GetPositionAtOffset(endOffset, LogicalDirection.Forward);
+
+                m_tags.Add(t);
                 //var s = flowDocument.ContentStart;
                 //while (flowDocument.ContentEnd.CompareTo(s) > -1)
                 //{
                 //    var x = s.GetPointerContext(LogicalDirection.Forward);
                 //    s = s.GetNextContextPosition(LogicalDirection.Forward);
                 //}         
-                do
-                {
-                    var context = start.GetPointerContext(LogicalDirection.Forward);
-                    while (context != TextPointerContext.Text)
-                    {
-                        if(context == TextPointerContext.ElementStart)
-                            ++startOffset;
-                        start = start.GetNextContextPosition(LogicalDirection.Forward);
-                        context = start.GetPointerContext(LogicalDirection.Forward);
-                    }
-                    textLength += start.GetTextRunLength(LogicalDirection.Forward);
-                    start = start.GetNextContextPosition(LogicalDirection.Forward);
-                } while (textLength < startPos);
 
-                var endOffset = startOffset;
-                while (textLength - startPos - word.Length < 0 && start != null)
-                {
-                    var context = start.GetPointerContext(LogicalDirection.Forward);
-                    while (context != TextPointerContext.Text && context != TextPointerContext.None)
-                    {
-                        if (context == TextPointerContext.ElementStart)
-                            ++endOffset;
-                        start = start.GetNextContextPosition(LogicalDirection.Forward);
-                        context = start.GetPointerContext(LogicalDirection.Forward);
-                    }
-                    textLength += start.GetTextRunLength(LogicalDirection.Forward);
-                    start = start.GetNextContextPosition(LogicalDirection.Forward);
-                }
+                //do
+                //{
+                //    var context = start.GetPointerContext(LogicalDirection.Forward);
+                //    while (context != TextPointerContext.Text)
+                //    {
+                //        if (context == TextPointerContext.ElementStart || context == TextPointerContext.ElementEnd)
+                //            ++startOffset;
+                //        start = start.GetNextContextPosition(LogicalDirection.Forward);
+                //        context = start.GetPointerContext(LogicalDirection.Forward);
+                //    }
+                //    textLength += start.GetTextRunLength(LogicalDirection.Forward);
+                //    start = start.GetNextContextPosition(LogicalDirection.Forward);
+                //} while (textLength < startPos);
 
-                Tag t = new Tag();
-                t.StartPosition = navigator.GetPositionAtOffset(startPos + startOffset, LogicalDirection.Forward);
-                t.EndPosition = navigator.GetPositionAtOffset(startPos + word.Length + endOffset, LogicalDirection.Forward);
-                t.Word = word;
-                m_tags.Add(t);
+                //while (textLength - startPos - word.Length < 0 && start != null)
+                //{
+                //    var context = start.GetPointerContext(LogicalDirection.Forward);
+                //    while (context != TextPointerContext.Text && context != TextPointerContext.None)
+                //    {
+                //        if (context == TextPointerContext.ElementStart)
+                //            ++endOffset;
+                //        start = start.GetNextContextPosition(LogicalDirection.Forward);
+                //        context = start.GetPointerContext(LogicalDirection.Forward);
+                //    }
+                //    textLength += start.GetTextRunLength(LogicalDirection.Forward);
+                //    start = start.GetNextContextPosition(LogicalDirection.Forward);
+                //}
 
-                text = text.Substring(position + trueSearchstring.Length);
+
+
+
+
+                //text = text.Substring(startPos + word.Length);
                 match = match.NextMatch();
             }
+        }
+
+        private List<TreeViewItemModel> GetTreeItem(FlowDocument document)
+        {
+            var list = new List<TreeViewItemModel>();
+            var start = document.ContentStart;
+            while (start != null && document.ContentEnd.CompareTo(start) > -1)
+            {
+                var model = new TreeViewItemModel();
+                var context = start.GetPointerContext(LogicalDirection.Forward);
+                //model.Header = context.ToString() + ":";
+                switch (context)
+                {
+                    case TextPointerContext.ElementStart:
+                        model.Header = context.ToString() + ":";
+                        //    start = start.GetNextContextPosition(LogicalDirection.Forward);
+                        //    model.Items = GetTreeItem(document ,ref start);
+                        list.Add(model);
+                        break;
+                    case TextPointerContext.None:
+                    case TextPointerContext.ElementEnd:
+                    //    return list;
+                    case TextPointerContext.EmbeddedElement:
+                    case TextPointerContext.Text:
+                        //default:
+                        model.Header = context.ToString() + ":";
+                        model.Header += start.GetTextInRun(LogicalDirection.Forward);
+                        list.Add(model);
+                        break;
+                }
+                start = start.GetNextContextPosition(LogicalDirection.Forward);
+            }
+            return list;
+        }
+
+        // Returns the offset for the specified position relative to any containing paragraph.
+        int GetOffsetRelativeToParagraph(TextPointer position)
+        {
+            // Adjust the pointer to the closest forward insertion position, and look for any
+            // containing paragraph.
+            Paragraph paragraph = (position.GetInsertionPosition(LogicalDirection.Forward)).Paragraph;
+
+            // Some positions may be not within any Paragraph; 
+            // this method returns an offset of -1 to indicate this condition.
+            return (paragraph == null) ? -1 : paragraph.ContentStart.GetOffsetToPosition(position);
+        }
+
+        // Returns a TextPointer to a specified offset into a specified paragraph. 
+        TextPointer GetTextPointerRelativeToParagraph(Paragraph paragraph, int offsetRelativeToParagraph)
+        {
+            // Verify that the specified offset falls within the specified paragraph.  If the offset is
+            // past the end of the paragraph, return a pointer to the farthest offset position in the paragraph.
+            // Otherwise, return a TextPointer to the specified offset in the specified paragraph.
+            return (offsetRelativeToParagraph > paragraph.ContentStart.GetOffsetToPosition(paragraph.ContentEnd))
+                ? paragraph.ContentEnd : paragraph.ContentStart.GetPositionAtOffset(offsetRelativeToParagraph);
         }
     }
 
